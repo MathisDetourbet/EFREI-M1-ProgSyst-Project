2db4f9eed86c8a5fb48be7f4266673ac5f31281b
Mathis Detourbet <mathis.detourbet@gmail.com>
2014-12-26 18:33:49 +0100

    Test1

---
+++ Prog_system_project/Prog_system_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Prog_system_project
+//
+//  Created by Mathis Detourbet on 26/12/2014.
+//  Copyright (c) 2014 Efrei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
